279c39c34d4fb63f6189678434034f8a71b69526
wichan <wichan7@naver.com>
2024-11-23 20:07:36 +0900

    style: layout, coin-flip

--- src/app/games/coin-flip/page.tsx
+++ src/app/games/coin-flip/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import style from "./page.module.css";
 import Button from "@/components/Button";
 import Input from "@/components/Input";
 import CoinFlipService from "@/services/coinFlipService";
@@ -20,22 +21,20 @@ export default function CoinFlipPage() {
   };
 
   const handleBet = async (isFront: boolean) => {
-    if (asset === 0) {
+    const _betAmount = betAmount;
+
+    if (_betAmount === 0) {
       return alert("0원은 배팅 안됨");
     }
-
-    if (betAmount > asset) {
+    if (_betAmount > asset) {
       return alert("가진 돈보다 더 많이 배팅 못함");
     }
-
-    const _betAmount = betAmount;
     addAsset(-_betAmount);
 
     const { reward, isFront: resultIsFront } = await doFlip({
       amount: _betAmount,
       isFront,
     });
-    console.log(reward, isFront, resultIsFront);
 
     if (isFront === resultIsFront) {
       addAsset(reward);
@@ -46,12 +45,14 @@ export default function CoinFlipPage() {
   };
 
   return (
-    <div>
+    <div className={style.container}>
       <Input value={betAmount} onChange={handleBetAmountChange} />
-      <Button onClick={() => handleBet(false)}>앞</Button>
-      <Button variant="secondary" onClick={() => handleBet(true)}>
-        뒤
-      </Button>
+      <div className={style.buttonContainer}>
+        <Button onClick={() => handleBet(false)}>앞</Button>
+        <Button variant="secondary" onClick={() => handleBet(true)}>
+          뒤
+        </Button>
+      </div>
     </div>
   );
 }
